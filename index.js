adb4c4b5201559ff63dca9520d13cdbc9e6cbb8c
crisperi <phoenixedian@gmail.com>
2024-03-16 18:30:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const rmName = document.getElementById("main");
+rmName.remove();
+
+const newHeader =document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Edian is the champion"
+document.append(newHeader);
